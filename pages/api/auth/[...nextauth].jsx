c22efa6631079d442d503300143257229f39c96d
bruno-neville-fcamara <bruno.santos@fcamara.com.br>
2022-08-23 20:30:43 -0300

    First commit

---
+++ pages/api/auth/[...nextauth].jsx
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import axios from "axios"
+import qs from 'qs'
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+        name: "Area do Doador",
+        credentials: {
+            email: { label: "Email", type: "email", placeholder: "Digite seu Email"},
+            password: {label: "Senha", type: "password"}
+        },        
+        async authorize(credentials, req){
+            const data = qs.stringify({
+              'username': credentials.email,
+              'password': credentials.password
+            })
+
+            const URL_BACKEND = 'http://localhost:8000'
+
+            const res = await axios({
+              method: "post",
+              url: `${URL_BACKEND}/client/sign_in`,
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+              },
+              data: data
+            })
+
+            if(res.status == 200){
+              return res.data
+            }
+            return null            
+        }
+    })
+  ],
+})
